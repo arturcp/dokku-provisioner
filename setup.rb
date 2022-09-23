85650207ff3515a1b15e2c1f91a8fc2f83e51f7e
Artur <artur.prado@gmail.com>
2022-09-23 19:19:35 -0300

    Add deploy instructions

--- setup.rb
+++ setup.rb
@@ -9,6 +9,8 @@ require_relative "models/dokku.rb"
 pastel = Pastel.new
 prompt = TTY::Prompt.new(active_color: :yellow, interrupt: :exit)
 
+divider = "============================================================================\n"
+
 app = prompt.ask("Name of the app:") { |q| q.required true }
 domain = prompt.ask("Domain:") { |q| q.required true }
 
@@ -27,7 +29,7 @@ https = prompt.yes?("Do you need a SSL certificate?")
 
 puts ""
 puts "Environment variables"
-puts "======================"
+puts divider
 puts ""
 puts "You can paste all your env vars at once. Make sure they are one per line "
 puts "and in the format KEY=value, like this:"
@@ -51,30 +53,45 @@ options = {
 instructions = Dokku.new(options).instructions
 
 puts ""
+puts ""
+puts ""
 puts "#{pastel.yellow.bold("TO CREATE YOUR APP")} \n"
-puts "==================================================\n"
-puts instructions[:create].join("\n")
+puts divider
+instructions[:create].each { |instruction| puts pastel.yellow(instruction) }
 
+deploy_instructions = instructions[:deploy].join("\n")
 puts ""
 puts "#{pastel.yellow.bold("TO DEPLOY YOUR APP")} \n"
-puts "==================================================\n"
-puts instructions[:deploy].join("\n")
+puts divider
+
+puts ""
+puts "You need to set up your local git config to point to a dokku remote."
+puts "If you do not have one setup, go to your project and run:"
+puts ""
+puts pastel.yellow(deploy_instructions)
+puts ""
+puts "If you do and need to change the remote url, run this instead:"
+puts ""
+puts pastel.yellow(deploy_instructions.gsub("remote add", "remote set-url"))
 
 if (instructions[:after_deploy].length > 0)
   puts ""
   puts "#{pastel.yellow.bold("AFTER THE DEPLOY")} \n"
-  puts "==================================================\n"
+  puts divider
+
   puts instructions[:after_deploy].join("\n")
 end
 
 if (instructions[:ssl].length > 0)
   puts ""
   puts "#{pastel.yellow.bold("SSL INSTRUCTIONS")} \n"
-  puts "==================================================\n"
+  puts divider
+
   puts instructions[:ssl].join("\n")
 end
 
 puts ""
 puts "#{pastel.yellow.bold("TO REMOVE YOUR APP")} \n"
-puts "==================================================\n"
+puts divider
+
 puts instructions[:destroy].join("\n")
