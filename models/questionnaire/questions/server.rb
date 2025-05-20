10a20f02959ecfffad2a51be35bb8488e432b789
Artur Caliendo Prado <artur.prado@gmail.com>
2025-05-20 09:33:58 -0300

    Allow names for servers (#13)

--- models/questionnaire/questions/server.rb
+++ models/questionnaire/questions/server.rb
@@ -9,8 +9,17 @@ module Questionnaire
 
       def ask
         if servers.length > 1
+          servers_hash = servers.each_with_object({}) do |server, hash|
+            ip, name = server.split("|", 2)
+            if name.nil? || name.empty?
+              hash[ip] = ip
+            else
+              hash[name] = ip
+            end
+          end
+
           server = prompt.select("To which server are you going to deploy your app?",
-            servers)
+            servers_hash)
         else
           server = servers.first || PLACEHOLDER
         end
